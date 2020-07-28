794d968e15be2f02882a97dc415d5f3d8f209092
amayuri <vanpaiayuri3@ezweb.ne.jp>
2020-07-28 23:48:58 +0900

    a

--- home.js
+++ home.js
@@ -67,3 +67,19 @@ $(function () {
 
     });
 });
+
+
+
+
+var mySwiper = new Swiper('.swiper-container', {
+	loop: true,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		type: 'bullets',
+		clickable: true
+	}
+});
