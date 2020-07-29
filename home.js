a41eb2e68b49c560bf309e80d17f7245e8aa9c04
amayuri <vanpaiayuri3@ezweb.ne.jp>
2020-07-29 20:37:20 +0900

    a

--- home.js
+++ home.js
@@ -1,3 +1,6 @@
+
+/*ヘッダー*/
+
 $(function(){
 	var _window = $(window),
 		_header = $('.header-menu'),
@@ -15,7 +18,6 @@ $(function(){
 	
 	_window.trigger('scroll');	
 });
-
 
 
 
@@ -42,34 +44,10 @@ $(function(){
 
 
 
-$(function () {
-    // slideshow クラスを持った要素ごとに処理を実行
-    $('.slideshow').each(function () {
-
-        var $slides = $(this).find('img');  // すべてのスライド
-        var slideCount = $slides.length;    // スライドの点数
-        var currentIndex = 0;               // 現在のスライドを示すインデックス
-
-        $slides.eq(currentIndex).fadeIn();  // 1 番目のスライドをフェードインで表示
-
-        // 3秒ごとに showNextSlide 関数を実行
-        setInterval(showNextSlide, 3000);
-
-        // 次のスライドを表示する関数
-        function showNextSlide () {
-            // 次に表示するスライドのインデックスを算出 (もし最後のスライドなら最初に戻る)
-            var nextIndex = (currentIndex + 1) % slideCount;
-            $slides.eq(currentIndex).fadeOut(); //現在のスライドをフェードアウト
-            $slides.eq(nextIndex).fadeIn();     // 次のスライドをフェードイン
-
-            currentIndex = nextIndex;           // 現在のスライドのインデックスを更新
-        }
-
-    });
-});
 
 
 
+/*スライドショー　前後ボタン*/
 
 var mySwiper = new Swiper('.swiper-container', {
 	loop: true,
