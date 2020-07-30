afe39b573979c70b5f1f8c3c2196b0e3e42494fa
amayuri <vanpaiayuri3@ezweb.ne.jp>
2020-07-30 22:46:55 +0900

    a

--- home.js
+++ home.js
@@ -1,43 +1,42 @@
-
 /*ヘッダー*/
 
 $(function(){
-	var _window = $(window),
-		_header = $('.header-menu'),
-		heroBottom;
-	
-	_window.on('scroll',function(){
-		heroBottom = $('.top-page').height();
-		if(_window.scrollTop() > heroBottom){
-			_header.addClass('transform');   
-		}
-		else{
-			_header.removeClass('transform');   
-		}
-	});
-	
-	_window.trigger('scroll');	
+    var _window = $(window),
+        _header = $('.header-menu'),
+        heroBottom;
+    
+    _window.on('scroll',function(){
+        heroBottom = $('.top-page').height();
+        if(_window.scrollTop() > heroBottom){
+            _header.addClass('transform');   
+        }
+        else{
+            _header.removeClass('transform');   
+        }
+    });
+    
+    _window.trigger('scroll');  
 });
 
 
 
 
 $(function(){
-	var _window = $(window),
-		_header = $('.header-menu a'),
-		heroBottom;
-	
-	_window.on('scroll',function(){
-		heroBottom = $('.top-page').height();
-		if(_window.scrollTop() > heroBottom){
-			_header.addClass('transform');   
-		}
-		else{
-			_header.removeClass('transform');   
-		}
-	});
-	
-	_window.trigger('scroll');	
+    var _window = $(window),
+        _header = $('.header-menu a'),
+        heroBottom;
+    
+    _window.on('scroll',function(){
+        heroBottom = $('.top-page').height();
+        if(_window.scrollTop() > heroBottom){
+            _header.addClass('transform');   
+        }
+        else{
+            _header.removeClass('transform');   
+        }
+    });
+    
+    _window.trigger('scroll');  
 });
 
 
@@ -50,14 +49,50 @@ $(function(){
 /*スライドショー　前後ボタン*/
 
 var mySwiper = new Swiper('.swiper-container', {
-	loop: true,
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev'
-	},
-	pagination: {
-		el: '.swiper-pagination',
-		type: 'bullets',
-		clickable: true
-	}
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+    }
+});
+
+
+
+
+
+//カーソル！！！！
+
+//idの付与
+var body = document.body;
+var cursor = document.createElement("div");
+var stalker = document.createElement("div");
+cursor.id = "cursor";
+stalker.id = "stalker";
+body.appendChild(cursor);
+body.appendChild(stalker);
+
+
+//マウスポインターの位置の取得
+body.addEventListener("mousemove", function(e) {
+    cursor.style.left = e.clientX + "px";
+    cursor.style.top = e.clientY + "px";
+    stalker.style.left = e.clientX + "px";
+    stalker.style.top = e.clientY + "px";
+  }, false);
+
+
+//a要素にhover時、activeのクラスを付与
+  $("a").on("mouseenter", function() {
+    $('#cursor').addClass("active");
+    $('#stalker').addClass("active");
+});
+ 
+$("a").on("mouseleave", function() {
+    $('#cursor').removeClass("active");
+    $('#stalker').removeClass("active");
 });
